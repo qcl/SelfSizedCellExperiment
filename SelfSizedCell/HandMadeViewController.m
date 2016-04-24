69a0cf54466dd55f5c477e420f3d601a31ac7624
Qing-Cheng Li <qc.linux@gmail.com>
2016-04-24 22:07:24 +0800

    Setup constraints for handmade  vc

--- SelfSizedCell/HandMadeViewController.m
+++ SelfSizedCell/HandMadeViewController.m
@@ -32,7 +32,20 @@
     self.title = @"純手工";
     self.view.backgroundColor = [UIColor redColor];
 
-    
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flowLayout];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [self.view addSubview:self.collectionView];
+
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0f constant:0.0f];
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0f constant:0.0f];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0f constant:0.0f];
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0f constant:0.0f];
+   
+    [NSLayoutConstraint activateConstraints:@[leading, top, bottom, trailing]];
 }
 
 @end
